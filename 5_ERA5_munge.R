b231e9267e94c12d075e320089e00b48e76c26e2
Karen - Work <kljorgenson@alaska.edu>
2025-08-12 12:39:33 -0800

    Restored files

---
+++ 5_ERA5_munge.R
@@ -0,0 +1,115 @@
+#### Covariate organization
+# Compile data from .csv file output from clipping ERA5 data to watersheds
+# Some parts of code must be adjusted for different climate variables
+library(tidyverse)
+library(tidyhydat)
+library(ggplot2)
+
+# Combine files for each year 
+filelist = list.files(path = "Output_data/", pattern = "*snowmelt*", # Adjust variable name
+                      full.names = TRUE, recursive = T)
+df_list <- lapply(filelist, read.csv) 
+names(df_list) <- basename( filelist )
+
+df_list_named <- map2(df_list, names(df_list), ~mutate(.x, source = .y))
+
+# Make all columns characters
+df_list_named <- lapply(df_list_named, function(x) {x[] <- lapply(x, as.character); x})
+cov.raw <- df_list_named %>% reduce(full_join)
+
+# Extract variable from file name
+cov.raw$variable <- substr(cov.raw$source, start = 6, stop = 200)
+cov.raw$variable <- gsub(".([0-9]+)|.nc.csv","",cov.raw$variable)
+
+# Remove duplicated time steps
+covs.hrly <- cov.raw %>% dplyr::select(date_UTC, time, station, variable, value) 
+
+# Check for duplicates in all but value
+any(duplicated(covs.hrly[,1:4]))
+
+names(covs.hrly) <- c("date_UTC", "hour_UTC", "station", "variable", "value")
+covs.hr <- covs.hrly
+
+### Take monthly averages
+# Based on timezone of discharge data
+
+# Match stations and UTC-X
+meta.ak <- read.csv("Data/river_metadata.csv") %>% filter(NameNom == "") %>% 
+  rename(station = StationNum) %>%
+  dplyr::select(station) %>% unique()
+meta.ak$UTC_minus <- 9 # All Alaska stations in same time zone
+meta.ak$station <- as.character(meta.ak$station)
+meta.can <- hy_stations() %>% filter(STATION_NUMBER %in% covs.hr$station) %>%
+  rename(station = STATION_NUMBER) %>%
+  mutate(UTC_minus = case_when(PROV_TERR_STATE_LOC %in% c("YT", "BC") ~ 8, # Adjust time zones for Canadian provinces
+                               PROV_TERR_STATE_LOC %in% c("NT", "AB", "NU") ~ 7,
+                               PROV_TERR_STATE_LOC %in% c("MB", "SK") ~ 6,
+                               PROV_TERR_STATE_LOC %in% c("ON", "QC") ~ 5,
+                               PROV_TERR_STATE_LOC %in% c("NL") ~ 4
+  )) %>% dplyr::select(station, PROV_TERR_STATE_LOC, UTC_minus)
+
+meta <- full_join(meta.ak, meta.can)
+
+# Adjust datetime from UTC to individual time zones
+covs.hr$station <- as.character(covs.hr$station)
+covs.hr <- left_join(covs.hr, meta)
+covs.hr$time <- ifelse(nchar(covs.hr$hour_UTC) == 1, paste(0,covs.hr$hour_UTC, sep = ""), covs.hr$hour_UTC)
+
+covs.hr$datetime_UTC <- as.POSIXct(paste(covs.hr$date_UTC, " ", covs.hr$time, ":00:00", sep = ""), format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
+covs.hr$datetime_adj <- covs.hr$datetime_UTC-covs.hr$UTC_minus* 3600
+covs.hr$date_adj <- as.Date(covs.hr$datetime_adj)
+covs.hr$year <- year(covs.hr$datetime_adj)
+covs.hr$month <- month(covs.hr$datetime_adj)
+
+### Calculate monthly average
+covs.hr$value <- as.numeric(covs.hr$value)
+covs.mo <- covs.hr %>% group_by(station, year, month, variable) %>% 
+  summarise(n = length(variable), value = if_else(first(variable) == 'temp', mean(value), sum(value))) %>% 
+  filter(n > 600) %>% dplyr::select(-c(n)) 
+
+# Export monthly data
+write.csv(covs.mo, "Output_data/Snowmelt_monthly.csv", row.names = F)
+
+# Daily
+covs.day <- covs.hr %>% mutate(jday = yday(datetime_adj)) %>% group_by(station, year, month, jday, variable) %>%
+  summarise(n = length(variable), value = if_else(first(variable) %in% c("temp", "temm", "temm-add.csv", "temm.nc-ad.csv"), mean(value), sum(value))) %>% 
+  filter(n == 24) %>% dplyr::select(-c(n))
+
+write.csv(covs.day, "Output_data/Snowmelt_daily.csv", row.names = F)
+
+
+
+
+################################################################
+
+#### Combine monthly data for all climate variables
+filelist = list.files(path = "Output_data/", pattern = "*monthly*", full.names = TRUE)
+filelist
+
+covars0 <- lapply(filelist, read.csv)
+covars <- lapply(covars0, function(x) {x[] <- lapply(x, as.character); x}) %>% reduce(full_join) %>% unique() 
+covars$value <- as.numeric(covars$value)
+covrs <- covars %>% filter(is.na(value) == F,is.na(station) == F,is.na(year) == F,is.na(month) == F)
+
+# Remove duplicated rows
+covrs <- covrs[!duplicated(covrs[,1:4]), ]
+
+# Check for duplicates in all but value
+any(duplicated(covrs[,1:4])) # station, year, month, variable
+
+# Make data wide and calculate rain
+covs.mo <- covrs %>% unique() %>%
+  pivot_wider(names_from = variable, values_from = value)  %>%
+  mutate(ppt2 = ifelse(ppt < 0, 0, ppt), # Replace negative precipitation with zero
+         snow2 = ifelse(snowfall < 0, 0, snowfall), # Replace negative snowfall with zero
+         snow3 = ifelse(ppt2 <= 0, 0, snow2), # If total precipitation is zero, make snowfall also zero
+         snow4 = ifelse(snow3 > ppt2, ppt2, snow3), # If snowfall is greater than total precipitation, make equal to total precipitation
+         rain = ppt2 - snow4, # Calculate rain
+         temp = temp-273.15) %>% select(station,year,month,ppt2,snow4,snowmelt,rain,temp) %>%
+  rename(snowfall = snow4, ppt = ppt2)
+
+# Add date
+covs.mo$date <- as.Date(paste(covs.mo$year, covs.mo$month, "15", sep = "-"), format = '%Y-%m-%d')
+
+# Export
+write.csv(co.mo, "Output_data/All_ERA5_monthly_AK_CAN.csv", row.names = F)
---
+++ 6_ERA5_compilation.R
@@ -0,0 +1,59 @@
+## Compile and clip ERA5 climate data
+# Some parts of code must be adjusted for each different variable
+
+library(ncdf4)
+library(raster) # package for raster manipulation
+library(sf) # package for geospatial analysis
+library(terra)
+library(tidyr)
+library(dplyr)
+library(data.table)
+
+# list files
+listfile <- list.files(path="Output_data/", pattern="Era5-temp-*", full.names = TRUE)
+listfile 
+
+#for(i in 33:length(listfile)){
+for(i in 1:32){
+## Covert to raster stack
+d0 <- rast(listfile[i])
+
+## Read in watershed boundaries from shapefile
+stations60 <- read.csv("Output_data/stations_historical.csv")%>% 
+  select(station) %>% unique()
+
+shape.watershed <- st_read("Data/Watershed_export/All_watersheds_final_9.shp") %>% 
+                  filter(StationNum %in% stations60$station) # For 1967-1998, only long-term stations
+
+# Fix invalid geometries
+shape.watershed <- st_make_valid(shape.watershed)
+
+# Reproject to match raster CRS
+shape.watershed <- st_transform(shape.watershed, crs(d0))
+
+# Convert to terra SpatVector
+shape_vect <- vect(st_zm(shape.watershed)) # Drop z dimension
+
+Sys.time()
+temp = raster::extract(d0, shape.watershed, method="simple", fun=mean, 
+                       exact = T #If TRUE the fraction of a cell that is covered is returned or used by fun
+)
+Sys.time()
+
+# Convert SpatialPolygonDataframe into data frame
+temp_df<- as.data.frame(temp)
+
+# Make data long
+temp_df <- gather(temp_df, dateUTC, value, 2:(ncol(temp_df)), factor_key = T) 
+temp_df$date0 <- substr(temp_df$dateUTC, 17, 40) # 15 for ppt and snowfall, 17 for snowmelt
+temp_df$date0 <- gsub("(.*)e\\.(.*)", "\\1\\e+\\2", temp_df$date0)
+temp_df$datetime_UTC <- as.POSIXct(as.numeric(temp_df$date0), origin = "1970-01-01", tz = "UTC")
+temp_df$date_UTC <- as.Date(temp_df$datetime_UTC)
+temp_df$time <- hour(temp_df$datetime_UTC)
+
+
+df <- data.frame(ID = unique(temp_df$ID), station = unique(shape_vect$StationNum))
+temp.dat <- merge(temp_df, df)
+
+write.csv(temp.dat, paste("Output_data/",substr(start = 72, stop = 200, listfile[i]),".csv", sep = ""), row.names = F)
+}
